8456d474e0e3c8d683bd2943a565398d537e4ee9
draggard11 <draggard2003@gmail.com>
2025-10-24 13:46:51 +0200

    started on PollOption.java and User.java

---
+++ src/main/java/no/hvl/dat250/feedapphvl/domain/PollOption.java
@@ -0,0 +1,42 @@
+package no.hvl.dat250.feedapphvl.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+public class PollOption {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Setter
+    private String title;
+
+    // default constructor
+    public PollOption() {}
+
+
+    /**
+     *
+     * @param voter the user that votes
+     * @return the vote that
+     */
+    public Vote addVote(User voter) {
+        return new Vote(voter, this);
+    }
+
+    public class Vote {
+        private final User voter;
+        private final PollOption poll;
+
+        public Vote(User voter, PollOption option) {
+            this.voter = voter;
+            this.poll = option;
+        }
+    }
+}
