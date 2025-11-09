ce0285afbed4d64717bbb53c3bc3888a96a3ce92
draggard11 <draggard2003@gmail.com>
2025-11-09 19:15:01 +0100

    continuing on other pc 2

---
+++ frontend/FeedApp/src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+    let { children } = $props();
+</script>
+
+<nav>
+    <a href="/login">login</a>
+    <a href="/register">register</a>
+</nav>
+
+{@render children()}
---
+++ frontend/FeedApp/src/routes/+page.svelte
@@ -0,0 +1,2 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit to read the documentation</p>
